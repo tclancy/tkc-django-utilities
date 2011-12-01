9ddc79d65b0ecc534366f304dcb31825ebdb2bcc
Tom Clancy <tclancy@gmail.com>
2011-12-01 12:47:51 -0500

    Added form field that accepts function for labelling options.

--- forms.py
+++ forms.py
@@ -31,3 +31,19 @@ class LoginForm(forms.Form):
             request.session.set_expiry(0)
             return True
         return False
+
+
+class CustomDisplayModelChoiceField(forms.ModelChoiceField):
+    def __init__(self, queryset, display_fx, empty_label=u"---------",
+                 cache_choices=False,
+                 required=True, widget=None, label=None, initial=None,
+                 help_text=None, to_field_name=None, *args, **kwargs):
+        self.display_fx = display_fx
+        
+        super(CustomDisplayModelChoiceField, self).__init__(queryset,
+                empty_label=u"---------", cache_choices=False,
+                required=True, widget=None, label=None, initial=None,
+                help_text=None, to_field_name=None, *args, **kwargs)
+    
+    def label_from_instance(self, obj):
+         return self.display_fx(obj)
